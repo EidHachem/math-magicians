ea1411137698f81d75fb62720958ab5a2b3e7590
EidHachem <eidhachem1@gmail.com>
2022-05-31 13:06:33 +0300

    Add Styling and routing functionality

--- src/App.js
+++ src/App.js
@@ -1,8 +1,36 @@
 import React from 'react';
+import { Routes, Route, Link } from 'react-router-dom';
+import './App.css';
 import Calculator from './components/Calculator';
+import Home from './components/Home';
+import Quote from './components/Quote';
 
 function App() {
-  return <Calculator />;
+  return (
+    <div>
+      <nav>
+        <p className="logo">
+          <Link to="/">Math Magician</Link>
+        </p>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="calculator">Calculator</Link>
+          </li>
+          <li>
+            <Link to="quote">Quote</Link>
+          </li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="calculator" element={<Calculator />} />
+        <Route path="quote" element={<Quote />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Quote.js
@@ -0,0 +1,14 @@
+import './Quote.css';
+
+const Quote = () => (
+  <p className="quote">
+    <q>
+      Mathematics is not a careful march down a well-cleared highway, but a journey into a strange
+      wilderness, where the explorers often get lost. Rigor should be a signal to the historians
+      that the maps have been made, and the real explorers have gone elsewhere
+    </q>
+    <p className="author">- W.S. Anglin</p>
+  </p>
+);
+
+export default Quote;
